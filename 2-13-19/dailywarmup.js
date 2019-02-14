313ab4d73478ee3c40bf5cd0ccd2c4e6f8c7cc9a
Brent Woodward <Brent.Woodward.1@gmail.com>
2019-02-13 22:02:44 -0800

    warmup complete

---
+++ 2-13-19/dailywarmup.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let arr = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10];
+
+let forLoop = (arr) => {
+  for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+function whileLoop(arr) {
+  let counter = 0;
+  while(counter < arr.length) {
+    console.log(arr[counter]);
+    counter++;
+  }
+};
+
+let mapper = (arr, cb) => {
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    newArr.push(cb(arr[i], i));
+  }
+  return newArr;
+}
+
+function filterer(a, c) {
+  let nA = [];
+  for (let i = 0; i < a.length; i++) {
+    if(c(a[i], i)) {
+      nA.push(a[i]);
+    }
+  }
+  return nA;
+};
+
+let reducer = (arr, cb, acc) => {
+  for(let i = 0; i < arr.length; i++) {
+    acc = cb(arr[i], i, acc);
+  }
+  return acc;
+};
+
+let state = {};
+let people = ['Kookla', 'Fran', 'Ollie'];
+let stuff = {
+  tv: 'huge',
+  radio: 'broken',
+  toothbrush: 'in use',
+  cars: ['Toyota', 'Mazda'],
+};
+
+let newPeople = ['Odie', ...people, 'Garfield'];
+let newStuff = {...stuff, cars: [...stuff.cars, 'Subaru']};
+let newState = {people: [...newPeople], stuff: {...newStuff}};
+
+console.log('people', people);
+console.log('state', state);
+console.log('stuff', stuff);
+console.log('new people', newPeople);
+console.log('new stuff', newStuff);
+console.log('new state', newState);
